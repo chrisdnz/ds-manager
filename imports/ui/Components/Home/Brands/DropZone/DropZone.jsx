dc9140436f0a6e64eccea0ea0ce69d7f5dfb255d
Óscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-01-04 22:35:13 -0600

    sube pero los borra

--- imports/ui/Components/Home/Brands/DropZone/DropZone.jsx
+++ imports/ui/Components/Home/Brands/DropZone/DropZone.jsx
@@ -1,86 +1,36 @@
 import React, {Component} from 'react';
-import Loader from 'react-loader';
+import Dropzone from 'react-dropzone';
 
 class DropZone extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            uploaded: true
-        }
         this.handleUpload = this.handleUpload.bind(this);
     }
-    componentDidMount() {
-        var componentRef = this;
-        $('#dnd').on('dragover', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            let dropZone = $('.jqDropZone'), foundDropZone;
-            let found = false, node = event.target;
-            do {
-                if($(node).hasClass('jqDropZone')) {
-                    found = true;
-                    foundDropZone = $(node);
-                    break;
-                }
-                node = node.parentNode;
-            } while (node != null);
-            dropZone.removeClass('in hover');
-            if(found) {
-                foundDropZone.addClass('hover');
-            }
-        });
-        $('#dnd').on('dragenter', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-        });
-        $('#dnd').on('drop', function(event) {
-            if(event.originalEvent.dataTransfer){
-                if(event.originalEvent.dataTransfer.files.length) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    componentRef.handleUpload(event.originalEvent.dataTransfer.files);
-                }   
-            }
-        });
-    }
+
     handleUpload(files) {
-        $(window).bind('beforeunload', function(){
-            return 'Su archivo no se ha guardado!'
+        _.each(files, function(file) {
+            file.owner = Meteor.userId(); //before upload also save the owner of that file
+            Images.insert(file, function(err, fileObj) {
+                if (err) {
+                    console.log(err); //in case there is an error, log it to the console
+                    toastr.error(err, "Error");
+                } else {
+                    console.log("imagen subida");
+                    toastr.success(fileObj._id, "Exito. Imagen Subida");
+                    //the image upload is done successfully.
+                    //you can use this callback to add the id of your file into another collection
+                    //for this you can use fileObj._id to get the id of the file
+                }
+            });
         });
-        this.setState({
-            uploaded: false
-        });
-        for(var i = 0; i < files.length; i++) {
-            Images.insert(files[i], function(err, image) {
-                let cursor = Images.find({_id: image._id});
-                let liveQuery = cursor.observe({
-                    changed: function(newImage, oldImage){
-                        if(newImage.isUploaded) {
-                            liveQuery.stop();
-                            console.log("Imagen subida");
-                        }
-                    }
-                })
-            })
-        }
 
     }
     render() {
-        let options = {
-            lines: 13,
-            color: '#fff',
-            rotate: 0,
-            direction: 1,
-            speed: 1,
-            top: '50%',
-            left: '50%',
-            position: 'absolute'
-        }
         return (
-            <div id="dnd" className="jqDropZone fade">
-                <Loader loaded={this.state.uploaded} options={options}>
-                    Arrastre aqui y deje caer el archivo
-                </Loader>
+            <div id="dnd">
+                <Dropzone onDrop={this.handleUpload}>
+                    <div>Try dropping some files here, or click to select files to upload.</div>
+                </Dropzone>
             </div>
         );
     }
