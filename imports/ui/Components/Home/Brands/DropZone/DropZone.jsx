03509f65e514694d62407f490de2129876dcb127
Óscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-01-05 10:02:10 -0600

    agregue preview de los archivos subidos

--- imports/ui/Components/Home/Brands/DropZone/DropZone.jsx
+++ imports/ui/Components/Home/Brands/DropZone/DropZone.jsx
@@ -3,34 +3,45 @@ import Dropzone from 'react-dropzone';
 
 class DropZone extends Component {
     constructor(props) {
-        super(props);
-        this.handleUpload = this.handleUpload.bind(this);
+        super(props)
+        this.state = {
+            files: []
+        }
+        this.onDrop = this.onDrop.bind(this);
     }
-
-    handleUpload(files) {
-        _.each(files, function(file) {
+    onDrop (files) {
+        this.setState({
+          files: files
+        });
+      console.log('Received files: ', files);
+      _.each(files, function(file) {
             file.owner = Meteor.userId(); //before upload also save the owner of that file
             Images.insert(file, function(err, fileObj) {
                 if (err) {
                     console.log(err); //in case there is an error, log it to the console
-                    toastr.error(err, "Error");
                 } else {
-                    console.log("imagen subida");
-                    toastr.success(fileObj._id, "Exito. Imagen Subida");
+                    toastr.success("Archivo Subido", "Exito");
                     //the image upload is done successfully.
                     //you can use this callback to add the id of your file into another collection
                     //for this you can use fileObj._id to get the id of the file
                 }
             });
         });
-
     }
+
     render() {
         return (
             <div id="dnd">
-                <Dropzone onDrop={this.handleUpload}>
-                    <div>Try dropping some files here, or click to select files to upload.</div>
+                <Dropzone
+                    multiple={true}
+                    accept="image/*"
+                    onDrop={this.onDrop}>
+                        <div>Arrastre y suelte las imagenes aqui para subir.</div>
                 </Dropzone>
+                {this.state.files.length > 0 ? <div>
+                <h2>Subiendo {this.state.files.length} archivos...</h2>
+                <div>{this.state.files.map((file) => <img height="84" width="84" key= {file.name} src={file.preview} /> )}</div>
+                </div> : null}
             </div>
         );
     }
