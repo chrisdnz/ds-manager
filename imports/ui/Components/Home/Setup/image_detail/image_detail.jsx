a9ca96f9592d9e4adf58440226cd37e2042cb822
Óscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-01-29 02:57:16 -0600

    ya cada imagen tiene su propio tiempo, solo hay que borrar y agregarlas de nuevo, falta pasar ese tiempo al carousel

---
+++ imports/ui/Components/Home/Setup/image_detail/image_detail.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import SliderTest from './SliderTest/SliderTest'
+import Alert from 'react-s-alert';
+
+class ImageDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+
+  render() {
+    return (
+      <div className='thumbnail'>
+        <img className='photo' src={`http://localhost:4000/cfs/files/Images/${this.props.codigo.Codigo}`}/>
+        <div className='caption'>
+          <SliderTest codigo = {this.props.codigo} tiempo = {this.props.codigo.Time/1000}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImageDetail;
