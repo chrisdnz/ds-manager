bf57ba2815fa87a5fb5c672cac8898e28a2edc6f
Christian Najera <chris_najera@unitec.edu>
2017-07-27 00:52:45 -0600

    arreglando bugs video html5

--- imports/ui/Components/cliente/App.jsx
+++ imports/ui/Components/cliente/App.jsx
@@ -8,52 +8,68 @@ class App extends Component {
     super(props);
     SELF = this;
   }
+  /**
+    Cambios en el componentDidUpdate,
+    el tiempo que guardamos en DB ya no importa
+    ahora lo que implemente es que el cycle se pause cada vez
+    que el video se pause, por eso estan los eventos propios de
+    html5 video, y si el video termina, pasa al siguiente slide,
+    cada vez que se pasa a un nuevo slide, la libreria ejecuta
+    before y after, entonces si el siguiente slide es un video,
+    hace lo mismo sino, toma el tiempo de la imagen
+
+    Lo que no funciona es que cuando es el ultimo slide y es un video,
+    se queda en un loop con ese ultimo video y no pasa de ahi...
+  **/
   componentDidUpdate() {
     if (this.props.tvsimages.length>1) {
       $('.carousel').cycle({
         fx: 'fade',
         speed: 500,
         timeoutFn: function (curr, next, opts, fwd) {
+          if (curr.tagName === 'VIDEO') {
+            curr.play();
+            return 0;
+          }
           return parseInt($(curr).attr('data-duration'));
         },
         after: function (curr, next, opts, fwd) {
-
-        },
-        before: function (curr, next, opts, fwd) {
-
-          let siguiente = this;
-          let actual = curr;
-          console.log('actual',actual);
-          console.log('siguiente',siguiente);
-          if (actual.tagName === 'VIDEO') {
-            let ended = setInterval(function () {
-              if (actual.ended) {
-                $('.carousel').cycle('resume');
-                if (siguiente.tagName === 'VIDEO') {
-                  siguiente.play();
-                }
-                clearInterval(ended);
-              } else {
-
-                $('.carousel').cycle('pause');
-              }
-            }, 1000);
-          } else {
-              
-            if (siguiente.tagName === 'VIDEO') {
-              siguiente.play();
+          console.log('after');
+          console.log(curr, next);
+          if (curr.tagName === 'VIDEO') {
+            curr.onwaiting = function() {
+              $('.myspinner').css('display', 'flex');
+              $('.carousel').cycle('pause');
+              console.log('loading video');
             }
+            curr.onplaying = function () {
+              $('.myspinner').css('display', 'none');
+              // $('.carousel').cycle('resume');
+              console.log('playing video');
+            }
+            curr.onended = function () {
+              $('.carousel').cycle('next');
+              $('.myspinner').css('display', 'none');
+              // $('.carousel').cycle('resume');
+              console.log('ended video');
+            }
+          } else {
+            $('.carousel').cycle('resume');
           }
-        },
+        }
       });
+    } else {
+      console.log('Only 1 media');
     }
-
   }
 
   render() {
     let i = 0;
     return (
       <div className="cliente-container">
+        <div className="myspinner">
+          <i className="fa fa-circle-o-notch fa-spin"></i>
+        </div>
         <div className="carousel">
           {this.props.tvname != undefined && (
             this.props.tvsimages.length > 0 ? (
